a48106b2356309f6d6ce72e7a57cf5311f0086db
mrlanu <mrlanu@gmail.com>
2020-05-19 21:10:54 -0500

    build(discovery-server): add discovery-server & init config

---
+++ discovery-server/src/test/java/io/lanu/warmsnow/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lanu.warmsnow.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
