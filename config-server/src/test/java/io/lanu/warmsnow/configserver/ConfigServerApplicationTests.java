e4ea3b33365e0ffb0a4285e6ed5d0ad0d626aad6
mrlanu <mrlanu@gmail.com>
2020-05-19 20:38:48 -0500

    build: init project

---
+++ config-server/src/test/java/io/lanu/warmsnow/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lanu.warmsnow.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
