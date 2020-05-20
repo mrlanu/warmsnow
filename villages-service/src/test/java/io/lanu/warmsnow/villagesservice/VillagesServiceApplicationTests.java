ab417f72918bcfe53646b5f5f094622ba06f2b89
mrlanu <mrlanu@gmail.com>
2020-05-19 22:36:16 -0500

    build(villages): add villages-service & init configuration
    connect to MongoDb (DB have to be up & running on Docker)
    >> cd ./docker
    >> docker-compose -f docker-compose-mongo.yml up

---
+++ villages-service/src/test/java/io/lanu/warmsnow/villagesservice/VillagesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lanu.warmsnow.villagesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillagesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
