885727445dc0e5e5ba30ce8f61c1f2be36dca2c5
mrlanu <mrlanu@gmail.com>
2020-05-20 20:31:35 -0500

    build(building-factory): add building-factory-service
    connect to MongoDb (DB have to be up & running on Docker)
    >> cd ./docker
    >> docker-compose -f docker-compose-mongo.yml up

---
+++ building-factory-service/src/test/java/io/lanu/warmsnow/building_factory_service/BuildingFactoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lanu.warmsnow.building_factory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingFactoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
