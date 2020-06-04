84b2ce1fd4229754b7cb580cf7f50fae5aba4b76
mrlanu <mrlanu@gmail.com>
2020-06-03 23:44:30 -0500

    build: add tasks-service module

---
+++ tasks-service/src/main/java/io/lanu/warmsbow/taskservice/TasksServiceApplication.java
@@ -0,0 +1,17 @@
+package io.lanu.warmsbow.taskservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class TasksServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TasksServiceApplication.class, args);
+    }
+
+}
