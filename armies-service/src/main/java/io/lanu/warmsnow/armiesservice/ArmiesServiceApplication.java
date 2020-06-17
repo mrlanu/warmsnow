5cc92ad69a79f6d9975066b5667222bac547dd2e
mrlanu <mrlanu@gmail.com>
2020-06-16 19:09:42 -0500

    build: add armies-service

---
+++ armies-service/src/main/java/io/lanu/warmsnow/armiesservice/ArmiesServiceApplication.java
@@ -0,0 +1,19 @@
+package io.lanu.warmsnow.armiesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableMongoAuditing
+public class ArmiesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArmiesServiceApplication.class, args);
+    }
+
+}
---
+++ armies-service/src/test/java/io/lanu/warmsnow/armiesservice/ArmiesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lanu.warmsnow.armiesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmiesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
