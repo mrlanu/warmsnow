b8387ef9e30e094222c79cba72d1099beb23b7ec
mrlanu <mrlanu@gmail.com>
2020-06-25 21:40:12 -0500

    build: add Constructions service

---
+++ constructions-service/src/main/java/io/lanu/warmsnow/constructionsservice/ConstructionsServiceApplication.java
@@ -0,0 +1,13 @@
+package io.lanu.warmsnow.constructionsservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableMongoAuditing
+public class ConstructionsServiceApplication {
+}
